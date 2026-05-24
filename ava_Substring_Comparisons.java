db6de96bda83fa95cc87b796ec8a35e4ff645dd2
Siddharth Phadatare <siddhu4285@gmail.com>
2026-05-25 01:23:50 +0530

    Create ava_Substring_Comparisons.java

---
+++ ava_Substring_Comparisons.java
@@ -0,0 +1,132 @@
+We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+Explanation 0
+
+String  has the following lexicographically-ordered substrings of length :
+
+We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring as two newline-separated values (i.e., ava\nwel).
+
+The stub code in the editor then prints ava as our first line of output and wel as our second line of output.
+
+Language
+Java 8
+More
+16171819202122231514131211109876541
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+    String smallest = "";
+    String largest = "";
+    smallest = largest = s.substring(0, k);
+
+    for (int i=1; i<s.length()-k+1; i++) {
+        String substr = s.substring(i, i+k);
+        if (smallest.compareTo(substr) > 0)
+
+Line: 11 Col: 45
+
+Test against custom input
+Java
+You have earned 10.00 points!
+You are now 2 points away from the 4th star for your java badge.
+97%148/150
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+
+Test case 6
+Compiler Message
+Success
+Input (stdin)
+welcometojava
+3
+Expected Output
+ava
+wel
+BlogScoringEnvironment
+
+
+
+  import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+    String smallest = "";
+    String largest = "";
+    smallest = largest = s.substring(0, k);
+
+    for (int i=1; i<s.length()-k+1; i++) {
+        String substr = s.substring(i, i+k);
+        if (smallest.compareTo(substr) > 0)
+            smallest = substr;
+        if (largest.compareTo(substr) < 0)
+            largest = substr;
+    }
+
+    return smallest + "\n" + largest;
+}
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+         
+        
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+      
